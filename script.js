a059da79813a1f84bf48d898299075618b43df4f
Shikhar Verma <89732561+ShikharVerma1922@users.noreply.github.com>
2023-09-18 00:36:10 +0530

    First github edit

--- script.js
+++ script.js
@@ -57,3 +57,4 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+// EDited using github
